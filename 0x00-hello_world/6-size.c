b65e1c0a434f4a3bc30a4883e3931f414b657c72
Sankaratom <bekalutilahun39@gmail.com>
2021-06-17 14:02:53 +0300

    lowlev

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -16,6 +16,6 @@ printf("Size of a char: %d byte(s)\n", sizeof(charvar));
 printf("Size of a int: %d byte(s)\n", sizeof(intvar));
 printf("Size of a long int: %d byte(s)\n", sizeof(longvar));
 printf("Size of a long long int: %d byte(s)\n", sizeof(llongvar));
-printf("Size of a float: %d byte(s)", sizeof(floatvar));
+printf("Size of a float: %d byte(s)\n", sizeof(floatvar));
 return (0);
 }
