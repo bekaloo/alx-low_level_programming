a65e9a707182f193275415bf45eb71908f464710
Sankaratom <bekalutilahun39@gmail.com>
2021-06-18 12:33:57 +0300

    c oxo1

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,25 +1,28 @@
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 /* more headers goes there */
-
-/** 
-*
+/**
 *main - Random number generator
-*Description - It generates a random numbers
-*Return - returns 0 if all goes well
+*Description: It generates a random numbers
+* Return: returns 0 if all goes well
  */
 int main(void)
 {
-	int n;
-
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
-    if (n>0)
-        printf("is positive");
-    else if(n<0)
-        printf("is negative");
-    else
-        printf("is zero");
-	return (0);
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if (n > 0)
+{
+printf("%d is positive", n);
+}
+else if (n < 0)
+{
+printf("%d is negative",n);
+}
+else
+{
+printf("%d is zero",n);
+}
+return (0);
 }
---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+*main - does something 
+*Description: snippet is from alx
+*Return: 0 if all goes well
+*/
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if the last digit of n is greater than 5: the string and is greater than 5
+if the last digit of n is 0: the string and is 0
+if the last digit of n is less than 6 and not 0: the string and is less than 6 and not 0
+	return (0);
+}
