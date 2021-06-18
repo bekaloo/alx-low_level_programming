f31b28842e2e8645d1d052cb32fa40291da4d885
Sankaratom <bekalutilahun39@gmail.com>
2021-06-18 19:28:20 +0300

    0x01-low

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+/**
+* main - Print combinations of two digit numbers
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int tens, tens1;
+int ones, ones1;
+
+for (tens = 0; tens <= 9; tens++)
+{
+for (ones = 0; ones <= 9; ones++)
+{
+for (ones1 = ones + 1; ones1 <= 9; ones1++)
+{
+putchar(tens + '0');
+putchar(ones + '0');
+putchar(' ');
+putchar(tens + '0');
+putchar(ones1 + '0');
+if (ones1 < 9)
+{
+putchar(',');
+putchar(' ');
+}
+
+}
+
+}
+
+}
+putchar('\n');
+
+return (0);
+}
