79c5b7494910ed2e8e7fcd24240a0ae0453084c4
Sankaratom <bekalutilahun39@gmail.com>
2021-06-18 20:35:00 +0300

    0x01-low

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,21 +6,23 @@
 */
 int main(void)
 {
-int tens;
+int tens, tens1;
 int ones, ones1;
 
 for (tens = 0; tens <= 9; tens++)
 {
 for (ones = 0; ones <= 9; ones++)
 {
+for (tens1 = 0; tens1 <= 9; tens1++)
+{
 for (ones1 = ones + 1; ones1 <= 9; ones1++)
 {
 putchar(tens + '0');
 putchar(ones + '0');
 putchar(' ');
-putchar(tens + '0');
+putchar(tens1 + '0');
 putchar(ones1 + '0');
-if (tens + '0' == '9' && ones1 + '0' == '9' && ones + '0' == '8')
+if (tens + '0' == '9' && ones1 + '0' == '9' && ones + '0' == '8' && tens1 + '0' == '9')
 {
 }
 else
@@ -28,7 +30,7 @@ else
 putchar(',');
 putchar(' ');
 }
-
+}
 }
 
 }
