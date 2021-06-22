3ea2f8478ecfdac7818b1cc45235679703198b42
Sankaratom <bekalutilahun39@gmail.com>
2021-06-22 23:24:12 +0300

    update

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -2,10 +2,11 @@
 /**
 * print_times_table - prints an nXn times table for
 * @n: the size of the times table
-*Description: n times table
+*Description: n times tablewhite space rem
 * Return: Always 0 (Success)
 */
-void print_times_table(int n) {
+void print_times_table(int n)
+{
 int i, j, p;
 if (n >= 0 && n <= 15)
 {
@@ -17,14 +18,12 @@ p = i * j;
 if (p <= 9)
 {
 if (j == 0)
-{
 _putchar(p % 10 + 48);
-}
 else
 {
 _putchar(' ');
 _putchar(' ');
-_putchar(p % 10 + 48);    
+_putchar(p % 10 + 48);
 }
 }
 else if (p >= 10 && p < 100)
