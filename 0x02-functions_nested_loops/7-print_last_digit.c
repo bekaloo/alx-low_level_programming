edfe41e506995898f86ef77cbde7c7ec94847771
Sankaratom <bekalutilahun39@gmail.com>
2021-06-22 23:41:21 +0300

    hot fix

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,11 +11,11 @@ int print_last_digit(int last_digit)
 if (last_digit >= 0)
 {
 _putchar(last_digit % 10 + 48);
-return (last_digit);
+return (last_digit % 10);
 }
 else
 {
 _putchar(-last_digit % 10 + 48);
-return (-last_digit);
+return (-last_digit % 10);
 }
 }
