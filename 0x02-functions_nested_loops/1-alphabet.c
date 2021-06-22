53b2a5261c6974fd63796f4ba3d4e93c0bc5884e
Sankaratom <bekalutilahun39@gmail.com>
2021-06-22 13:42:05 +0300

    functions and nested loops

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,5 @@
 #include "holberton.h"
 #include "_putchar.c"
-void print_alphabet(void);
-/**
-*main - execution starts there
-*Description: prints all small letter alphabets
-*Return: zero ig successful
-*/
-int main(void)
-{
-print_alphabet();
-return (0);
-}
 /**
 *print_alphabet - prints small letter alphabet
 */
