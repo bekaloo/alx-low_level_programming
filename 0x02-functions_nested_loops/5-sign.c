95f0809ec55e8d56459c48f851cad0769eea176b
Sankaratom <bekalutilahun39@gmail.com>
2021-06-23 00:02:52 +0300

    hot fix

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,18 +1,18 @@
 #include "holberton.h"
 /**
-*print_sign - prints the sign of a given int 
+*print_sign - prints the sign of a given int
 *@n: argument to be checked if negative or not
-*Description:prints the sign of a given int 
+*Description:prints the sign of a given int
 *Return: the -1 if -ve, 1 if +ve , 0 if 0
 */
 int print_sign(int n)
 {
-if(n < 0)
+if (n < 0)
 {
 _putchar(45);
 return (-1);
 }
-else if(n==0)
+else if (n == 0)
 {
 _putchar(48);
 return (0);
