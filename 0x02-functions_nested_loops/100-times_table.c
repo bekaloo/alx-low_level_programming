b624e9c5ebb482c2fda846c107ad6ad3d2f359a0
Sankaratom <bekalutilahun39@gmail.com>
2021-06-22 22:41:02 +0300

    update

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,6 +7,8 @@
 */
 void print_times_table(int n) {
 int i, j, p;
+if (n >= 0 && n <= 15)
+{
 for (i = 0; i <= n; i++)
 {
 for (j = 0; j <= n; j++)
@@ -45,6 +47,7 @@ _putchar(' ');
 else
 {
 _putchar('\n');
+}
 }
 }
 }
