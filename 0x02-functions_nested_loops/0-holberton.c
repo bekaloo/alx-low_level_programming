c8b7eefdc9163befb48886429361935edba6b903
Sankaratom <bekalutilahun39@gmail.com>
2021-06-22 09:53:31 +0300

    printing holberton

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+*main - prints holberton
+*Description: printing the name of the company
+*Return: 0 if all goes well
+*/
+int main(void)
+{
+printf("Holberton\n");
+return (0);
+}
