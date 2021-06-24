e232283b9b3b4c46ef0b72ea43607dd1dc119575
Sankaratom <bekalutilahun39@gmail.com>
2021-06-24 15:03:56 +0300

    Printing diagonal line

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,7 +1,18 @@
 #include "holberton.h"
 /**
-*
+*print_diagonal - prints spades diagonally
+*@n: how many lines
 *Description:
 *Return:
 */
-
+void print_diagonal(int n)
+{
+int i, j;
+for (i = 0; i < n; i++)
+{
+for (j = 0; j < i; j++)
+{
+_putchar(' ');
+}
+}
+}
