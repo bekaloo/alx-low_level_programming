dbc7c26843fece16b90ae5669220903511b00a4e
Sankaratom <bekalutilahun39@gmail.com>
2021-06-24 20:23:14 +0300

    Hot fix

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,7 +1,23 @@
-#include "holberton.h"
+#include <stdio.h>
 /**
-*
+* main - beginning of execution
 *Description:
-*Return:
+*Return: 0 - success
 */
 
+int main(void)
+{
+long i, number = 612852475143;
+
+for (i  = 2; i <= number; i++)
+{
+if (number % i == 0)
+{
+number = number / i;
+i--;
+}
+}
+printf("%lu\n", i);
+return (0);
+
+}
