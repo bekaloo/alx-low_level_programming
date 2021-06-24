0e6081a96a95333fcb23b7f671a27a4ba8bca15a
Sankaratom <bekalutilahun39@gmail.com>
2021-06-24 18:04:20 +0300

    Hot fix

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,7 +1,44 @@
 #include "holberton.h"
+#include <stdio.h>
 /**
-*
+* main - executes
 *Description:
-*Return:
+*Return: Always 0 (Success)
 */
 
+int main(void)
+{
+int i = 1;
+for (; i <= 100; i++)
+{
+if (i % 3 == 0 && i % 5 == 0)
+{
+printf("FizzBuzz ");
+}
+else
+{
+if(i % 3 == 0)
+{
+printf("Fizz ");
+}
+else if (i % 5 == 0)
+{
+if(i == 100)
+{
+printf("Buzz");
+}
+else
+{
+printf("Buzz ");
+}
+
+}
+else
+{
+    printf("%d ", i);
+}
+}
+}
+printf('\n');
+return (0);
+}
