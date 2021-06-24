dac37cb6da3d7de3c91f0f3ef44368cfe7950db0
Sankaratom <bekalutilahun39@gmail.com>
2021-06-24 16:20:03 +0300

    Hot fix

--- 0x04-more_functions_nested_loops/dummy.c
+++ 0x04-more_functions_nested_loops/dummy.c
@@ -1,27 +1,5 @@
 #include <stdio.h>
-// void print_diagonal(int n)
-// {
-// int i, j;
-// if (n > 0)
-// {
-// for (i = 0; i < n; i++)
-// {
-// for (j = 0; j <= i; j++)
-// {
-// putchar(' ');
-// if (j == i)
-// {
-// putchar('\\');
-// }
-// }
-// putchar('\n');
-// }
-// }
-// else
-// {
-// putchar('\n');
-// }
-// }
+
 void print_line(int n)
 {
 int i;
