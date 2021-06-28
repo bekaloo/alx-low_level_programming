550e71c817557bbd377c225944c330b083c7524f
Sankaratom <bekalutilahun39@gmail.com>
2021-06-28 15:17:34 +0300

    reset to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+* reset_to_98 - sets the value of its parameter to 98
+* @n: is a pointer
+*Description: value setter
+*Return: nothing
+*/
+
+void reset_to_98(int *n)
+{
+*n = 98;
+}
