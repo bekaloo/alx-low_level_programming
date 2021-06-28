ac512316b330bcd09369718180846682a64e6859
Sankaratom <bekalutilahun39@gmail.com>
2021-06-28 18:11:14 +0300

    string printer

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,7 +1,20 @@
 #include "holberton.h"
 /**
-*
-*Description:
+* _puts - holbertons puts
+* @str: string that gets passed to the _puts function
+*Description: prints a char
 *Return:
 */
+void _puts(char *str)
+{
+int i = 0, j;
+while (s[i])
+{
+i++;
+}
+for (j = 0; j < i; j++)
+{
+_putchar(s[j]);
+}
+}
 
