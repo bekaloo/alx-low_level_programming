f48066d83c872bbc03b1a02b8c97de7f6fab8b4a
Sankaratom <bekalutilahun39@gmail.com>
2021-06-28 18:20:14 +0300

    String reverser

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,7 +1,20 @@
 #include "holberton.h"
 /**
-*
-*Description:
-*Return:
+* print_rev - string reversing function
+*@s: string to be reversed
+*Description: string reversing function
+*Return: void so nothing
 */
-
+void print_rev(char *s)
+{
+int i = 0, j;
+while (s[i])
+{
+i++;
+}
+for (j = i - 1; j >= 0; j--)
+{
+_putchar(s[j]);
+}
+_putchar('\n');
+}
