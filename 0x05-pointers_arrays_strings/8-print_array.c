75226d5171945fa6bb2dedf025e3d92ce1f13a88
Sankaratom <bekalutilahun39@gmail.com>
2021-06-29 01:50:41 +0300

    prints_a_defined_portion_of_an_array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,7 +1,24 @@
 #include "holberton.h"
+#include <stdio.h>
 /**
-*
+* print_array - prints a portion of the array
+* @a: the array of integers
+* @n: the number of array elements to print
 *Description:
-*Return:
+*Return: void
 */
-
+void print_array(int *a, int n)
+{
+int i = 0;
+for (i = 0; i < n; i++)
+{
+if (i < n - 1)
+{
+printf("%d, ", a[i]);
+}
+else
+{
+printf("%d", a[i]);
+}
+}
+}
