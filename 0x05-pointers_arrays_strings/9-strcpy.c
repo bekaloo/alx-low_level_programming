6f7eb380762c3fb769f4a98ff71623fb6eefd95a
Sankaratom <bekalutilahun39@gmail.com>
2021-06-29 02:08:51 +0300

    string_copier

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,7 +1,14 @@
 #include "holberton.h"
 /**
-*
+* _strcpy - string copier...more like assigner
+* @dest: target
+* @src: source
 *Description:
-*Return:
+*Return: string
 */
 
+char *_strcpy(char *dest, char *src)
+{
+dest = src;
+return (dest);
+}
