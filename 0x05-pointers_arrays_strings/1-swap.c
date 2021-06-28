efa952af2cab4cf98f978ea2aadde19527f732f3
Sankaratom <bekalutilahun39@gmail.com>
2021-06-28 17:45:56 +0300

    swapper

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,7 +1,16 @@
 #include "holberton.h"
 /**
-*
-*Description:
-*Return:
+* swap_int - function that swaps the int values
+*@a: first integer
+*@b: second integer
+*Description: swapper
+*Return: nothing
 */
+void swap_int(int *a, int *b)
+{
+int tmp;
+tmp = *a;
+*a = *b;
+*b = tmp;
+}
 
