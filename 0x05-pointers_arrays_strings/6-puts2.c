e7f3d946ae95f9d528014a5a6cf65306aa50575f
Sankaratom <bekalutilahun39@gmail.com>
2021-06-29 00:56:27 +0300

    print vertically

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,7 +1,20 @@
 #include "holberton.h"
 /**
-*
+* puts2 - printing string chars on different lines of
+* @str: string
 *Description:
-*Return:
+*Return: nothing
 */
-
+void puts2(char *str)
+{
+int i = 0, j;
+while (str[i])
+{
+i++;
+}
+for (j = i - 1; j >= 0; j--)
+{
+_putchar(str[j]);
+_putchar('\n');
+}
+}
