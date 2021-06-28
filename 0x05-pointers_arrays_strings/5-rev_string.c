040615c71ec5c9cefc6e7151c0204836b8bf97d1
Sankaratom <bekalutilahun39@gmail.com>
2021-06-28 20:25:47 +0300

    string reverser

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,7 +1,22 @@
 #include "holberton.h"
 /**
-*
+* rev_string - string reverser
+* @s: strubg to be reversed
 *Description:
-*Return:
+*Return: void
 */
-
+void rev_string(char *s)
+{
+int i = 0, j, k = 0;
+while (s[i])
+{
+i++;
+}
+char tmp[i];
+for (j = i - 1; j >= 0; j--)
+{
+s[j] = tmp[k];
+k++;
+}
+*s = tmp;
+}
