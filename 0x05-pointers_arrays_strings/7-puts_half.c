397e3afa7fe1a4ae22676ab483272eed386a0b29
Sankaratom <bekalutilahun39@gmail.com>
2021-06-29 01:26:54 +0300

    prints_chars_if_they_have_an_even_index

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,7 +1,20 @@
 #include "holberton.h"
 /**
-*
+* puts_half - prints half of the string
+* @str: string
 *Description:
-*Return:
+*Return: nothing
 */
-
+void puts_half(char *str)
+{
+int i = 0, j;
+while (str[i])
+{
+i++;
+}
+for (j = i / 2; j < i; j--)
+{
+_putchar(str[j]);
+}
+_putchar('\n');
+}
