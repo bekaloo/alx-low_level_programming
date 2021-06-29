5749fbab0f1835ff2974aae49c1da87d293bbb31
Sankaratom <bekalutilahun39@gmail.com>
2021-06-29 06:49:26 +0300

    string to integer converter

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,28 +1,35 @@
 #include "holberton.h"
 /**
- * _strlen - strlen alternative representation
- * @s: the char array or string
- *Description: strlen alt
- *Return: the length of the string
- */
-int _atoi(char *s) {
-  int i = 0, j, k, tmp, conv = 0, c = 0;
-  while (s[i]) {
-    if (s[i] >= 48 && s[i] <= 57) {
-      continue;
-    } else {
-      return (0);
-      break;
-    }
-    i++;
-  }
-  for (j = i; j > 0; j--) {
-    tmp = 1;
-    for (k = 0; k < j - 1; k++) {
-      tmp *= 10;
-    }
-    conv +=  (s[c] - 48) * tmp;
-    c++;
-  }
-  return (conv);
+* _atoi - strlen alternative representation
+* @s: the string about to be converted
+*Description: str to int
+*Return: integer if success, 0 if bad parameter
+*/
+int _atoi(char *s)
+{
+int i = 0, j, k, tmp, conv = 0, c = 0;
+while (s[i])
+{
+if (s[i] >= 48 && s[i] <= 57)
+{
+continue;
+}
+else
+{
+return (0);
+break;
+}
+i++;
+}
+for (j = i; j > 0; j--)
+{
+tmp = 1;
+for (k = 0; k < j - 1; k++)
+{
+tmp *= 10;
+}
+conv +=  (s[c] - 48) * tmp;
+c++;
+}
+return (conv);
 }
