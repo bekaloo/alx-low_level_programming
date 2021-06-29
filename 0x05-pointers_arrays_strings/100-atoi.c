f9049fb2cadc5facad0a97b1e80909c212066a19
Sankaratom <bekalutilahun39@gmail.com>
2021-06-29 20:16:01 +0300

    int printer

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,5 +1,24 @@
 #include "holberton.h"
 /**
+* sign_counter - strlen alternative representation
+* @s: the string about to be converted
+* @sc: count
+*Description: str to int
+*Return: integer if success, 0 if bad parameter
+*/
+int sign_counter(int sc, char *s)
+{
+int l, neg = 0;
+for (l = sc; l >= 0; l--)
+{
+if (s[l] == '-')
+{
+neg++;
+}
+}
+return (neg);
+}
+/**
 * _atoi - strlen alternative representation
 * @s: the string about to be converted
 *Description: str to int
@@ -7,11 +26,19 @@
 */
 int _atoi(char *s)
 {
-int i = 0, j, k, tmp, conv = 0, c = 0;
+int i = 0, j, k, tmp, conv = 0, c = 0, sc = 0, l, neg = 0;
 while (s[i])
 {
 if (s[i] >= 48 && s[i] <= 57)
 {
+if (sc > 0)
+{
+
+}
+else
+{
+sc = i;
+}
 }
 else
 {
@@ -27,8 +54,13 @@ for (k = 0; k < j - 1; k++)
 {
 tmp *= 10;
 }
-conv +=  (s[c] - 48) * tmp;
+conv += (s[c] - 48) * tmp;
 c++;
+}
+sign_counter(sc, s);
+if (neg % 2 != 0)
+{
+conv = -conv;
 }
 return (conv);
 }
