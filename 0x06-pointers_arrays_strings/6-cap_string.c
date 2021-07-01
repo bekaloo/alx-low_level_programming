80885b32f75a87f27cf6c5c2348e0db6415aafd5
Sankaratom <bekalutilahun39@gmail.com>
2021-07-01 17:21:47 +0300

    string to upper

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,7 +1,36 @@
 #include "holberton.h"
 /**
-*
+* cap_string - capitalizes words
+* @c: the string to be
 *Description:
-*Return:
+*Return: capitalized version of the parameter string
 */
-
+char *cap_string(char *c)
+{
+int i = 0;
+while (c[i])
+{
+switch (c[i])
+{
+case ',':
+case ';':
+case '.':
+case '!':
+case '?':
+case '"':
+case '(':
+case ')':
+case '{':
+case '}':
+case ' ':
+case '\n':
+case '\t':
+if (c[i + 1] > 96 && c[i + 1] < 123)
+{
+c[i + 1] = c[i + 1] - 32;
+}
+}
+i++;
+}
+return (c);
+}
