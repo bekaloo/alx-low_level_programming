97ee528c74586eba9e23e622048b431a135780af
Sankaratom <bekalutilahun39@gmail.com>
2021-07-01 15:33:16 +0300

    integer array reverser

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,7 +1,20 @@
 #include "holberton.h"
 /**
-*
+* reverse_array - integer array reverser
+* @a: the array to be reversed
+* @n: the number of elements the array contains
 *Description:
 *Return:
 */
 
+void reverse_array(int *a, int n)
+{
+int cnt, tmp, rcnt = n - 1;
+for (cnt = 0; cnt < n / 2; cnt++)
+{
+tmp = a[cnt];
+a[cnt] = a[rcnt];
+a[rcnt] = tmp;
+rcnt--;
+}
+}
