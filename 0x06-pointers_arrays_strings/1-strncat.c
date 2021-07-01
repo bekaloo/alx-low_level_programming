9d2b07f90895697967fed7a16dcdc683842713d5
Sankaratom <bekalutilahun39@gmail.com>
2021-07-01 13:57:12 +0300

    holberton.h update

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -33,4 +33,5 @@ scntr++;
 }
 }
 dest = cnctd;
+return (dest);
 }
--- 0x06-pointers_arrays_strings/dummy.c
+++ 0x06-pointers_arrays_strings/dummy.c
@@ -1,38 +1,31 @@
 #include<stdio.h>
 
-char *_strcat(char *dest, char *src)
+char *_strcat(char *dest, char *src, int n)
 {
-int i = 0, j = 0, scntr = 0, cntr;
-while (src[i]) {
-i++;
-}
-while(dest[j])
-{
-j++;
-}
-char cnctd[i + j];
-for(cntr = 0;cntr < i + j; cntr++)
-{
-if(cntr<j)
-{
-cnctd[cntr] = dest[cntr];
-}
-else 
-{
-cnctd[cntr]=src[scntr];
-scntr++;
-}
-}
-printf("%d\n",i+j);
-dest = cnctd;
-return (dest);
+	int i;
+	int j;
+
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	while (j < n && src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
 }
 
 
 
 int main(){
     
-    char* a = _strcat("coder"," network");
+    char* a = _strcat("coder"," network",5);
     
     printf("%s",a);
     
