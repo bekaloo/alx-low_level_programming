e09ce896493b50d384bcb7b7be0f4b0f00f60ab6
Sankaratom <bekalutilahun39@gmail.com>
2021-07-01 15:41:32 +0300

    string to upper

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,7 +1,20 @@
 #include "holberton.h"
 /**
-*
+* string_toupper - capitalizes all characters in a string
+* @c: the string to be capitalized
 *Description:
-*Return:
+*Return: capitalized string
 */
 
+char *string_toupper(char *c)
+{
+int i = 0;
+while (c[i])
+{
+if (c[i] >= 97 && c[i] <= 122)
+{
+c[i] = 'A' + (c[i] - 97);
+}
+}
+return (c);
+}
