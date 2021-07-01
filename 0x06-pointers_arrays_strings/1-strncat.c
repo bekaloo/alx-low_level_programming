fe9e1ef5d4328776ac0ec31a3d1444138d6fe51d
Sankaratom <bekalutilahun39@gmail.com>
2021-07-01 14:03:58 +0300

    updating strcat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,27 +1,30 @@
 #include "holberton.h"
 /**
-*
+* _strncat - string appender
+* @dest: destination string
+* @src: source string
+* @n: number of characters to be appended on the destination string
 *Description:
-*Return:
+*Return: a longer string
 */
 
 char *_strncat(char *dest, char *src, int n)
 {
-	int i;
-	int j;
+int i;
+int j;
 
-	i = 0;
-	while (dest[i] != '\0')
-	{
-		i++;
-	}
-	j = 0;
-	while (j < n && src[j] != '\0')
-	{
-		dest[i] = src[j];
-		i++;
-		j++;
-	}
-	dest[i] = '\0';
-	return (dest);
+i = 0;
+while (dest[i] != '\0')
+{
+i++;
+}
+j = 0;
+while (j < n && src[j] != '\0')
+{
+dest[i] = src[j];
+i++;
+j++;
+}
+dest[i] = '\0';
+return (dest);
 }
