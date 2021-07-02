09da340553551577386090ed932e6039b4631024
Sankaratom <bekalutilahun39@gmail.com>
2021-07-02 04:40:33 +0300

    all at once

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,9 +1,12 @@
 #include "holberton.h"
 /**
-*
-*Description:
-*Return:
-*/
+ * _strncpy - copy a string
+ * @dest: input value
+ * @src: input value
+ * @n: input value
+ *
+ * Return: dest
+ */
 
 char *_strncpy(char *dest, char *src, int n)
 {
