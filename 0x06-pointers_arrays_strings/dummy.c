29ad4f8251418439127d421b00b76afe69edab5d
Sankaratom <bekalutilahun39@gmail.com>
2021-07-03 04:08:20 +0300

    all at once

--- 0x06-pointers_arrays_strings/dummy.c
+++ 0x06-pointers_arrays_strings/dummy.c
@@ -2,20 +2,35 @@
 
 void print_number(int n)
 {
-int dig = 1, i;
-while (n / dig >= 1)
+int copy, nth, size = 1, ones = n % 10;
+
+n /= 10;
+copy = n;
+if (ones < 0)
 {
-dig *= 10;
+ones *= -1, copy *= -1, n *= -1;
+putchar('-');
 }
-while (n > 0) {
-putchar('0' + (n % dig));
-dig /= 10;
+if (copy > 0)
+{
+while (copy / 10 != 0)
+{
+copy /= 10, size *= 10;
 }
+while (size > 0)
+{
+nth = n / size;
+putchar('0' + nth);
+n = n - (nth *size);
+size /= 10;
+}
+}
+putchar('0' + ones);
 }
 
 int main(){
     
-    print_number(6575);
+    print_number(-6);
     
     return 0;
 }
