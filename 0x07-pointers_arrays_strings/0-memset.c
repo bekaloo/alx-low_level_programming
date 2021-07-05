5248a787e682c437332cab98b018c3588aa6333b
Sankaratom <bekalutilahun39@gmail.com>
2021-07-05 18:45:27 +0300

    0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+*_memset - sets a range of adresses to a value
+*@s: string to copy to
+*@b: value to be copied
+*@n: number of elements to be set
+*Description:
+*Return: s
+*/
+
+char *_memset(char *s, char b, unsigned int n)
+{
+int i;
+for (i = 0; i < n; i++)
+{
+s[i] = b;
+}
+return (s);
+}
