2d07303084f254efc35e0f107fa4bf22bf25ced7
Sankaratom <bekalutilahun39@gmail.com>
2021-07-07 01:42:57 +0300

    second deadline is here yow

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,7 +1,35 @@
 #include "holberton.h"
 /**
-*
+* _strstr - searching a string
+* @haystack: text
+* @needle: search string
 *Description:
-*Return:
+*Return: address of string start
 */
 
+char *_strstr(char *haystack, char *needle)
+{
+int i, j, nl = 0;
+while (needle[nl])
+{
+nl++;
+}
+for (i = 0; haystack[i]; i++)
+{
+if (needle[0] == haystack[i])
+{
+for (j = 1; needle[j] && haystack[i + j]; j++)
+{
+if (needle[j] != haystack[i + j])
+{
+break;
+}
+}
+if(j == nl)
+{
+return (&haystack[i]);
+}
+}
+}
+return (0);
+}
--- 0x07-pointers_arrays_strings/_memcpy.c
+++ 0x07-pointers_arrays_strings/_memcpy.c
@@ -2,33 +2,41 @@
 #include "3-strspn.c"
 #include <stdio.h>
 
-void print_chessboard(char (*a)[8])
+char *_strstr(char *haystack, char *needle)
 {
-int i;
-while (a[i])
+int i, j, nl = 0;
+while (needle[nl])
 {
-putchar(*a[0]);
-if (i % 8 == 0)
-{
-putchar('\n');
+nl++;
 }
-i++;
+for (i = 0; haystack[i]; i++)
+{
+if (needle[0] == haystack[i])
+{
+for (j = 1; needle[j] && haystack[i + j]; j++)
+{
+if (needle[j] != haystack[i + j])
+{
+break;
 }
+}
+if(j == nl)
+{
+return (&haystack[i]);
+}
+}
+}
+return (0);
 }
 
 int main(void)
 {
     
-    char board[8][8] = {
-        {'r', 'k', 'b', 'q', 'k', 'b', 'k', 'r'},
-        {'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
-        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-        {'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
-        {'R', 'K', 'B', 'Q', 'K', 'B', 'K', 'R'},
-    };
-    print_chessboard(board);
+    char *s = "hello, world";
+    char *f = "world";
+    char *t;
+
+    t = _strstr(s, f);
+    printf("%s\n", t);
     return (0);
 }
