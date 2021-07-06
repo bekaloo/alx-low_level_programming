405a58836ae6ecfacab3c347116ce1afe16e00d1
Sankaratom <bekalutilahun39@gmail.com>
2021-07-06 15:07:47 +0300

    diag adder

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,7 +1,22 @@
-#include "holberton.h"
+#include <stdio.h>
 /**
-*
+* print_diagsums - printing the sum of the diagonal elements of a matrix
+* @a: Matrix
+* @size: size of the matrix
 *Description:
-*Return:
+*Return: void
 */
 
+void print_diagsums(int *a, int size)
+{
+int i, ltr = 0, rtl = 0, s = size * size;
+for (i = 0; i < s; i += size + 1)
+{
+ltr += a[i];
+}
+for (i = size - 1; i <= s - size; i += size - 1)
+{
+rtl += a[i];
+}
+printf("%d, %d\n", ltr, rtl);
+}
