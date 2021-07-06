cd9cfbec669ded1e5d7dcd5e3aef9b8ce7a505d5
Sankaratom <bekalutilahun39@gmail.com>
2021-07-06 15:42:37 +0300

    strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,7 +10,7 @@
 char *_strchr(char *s, char c)
 {
 int i = 0;
-while (s[i] != '\0' && c != '\0')
+while (s[i])
 {
 if (s[i] == c)
 {
@@ -18,5 +18,6 @@ return (&s[i]);
 }
 i++;
 }
+
 return (0);
 }
