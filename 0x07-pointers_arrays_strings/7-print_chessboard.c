cfbdde88d0870d09f6285d0a8920f8da0a5a60bb
Sankaratom <bekalutilahun39@gmail.com>
2021-07-06 16:34:41 +0300

    chessboard printer

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,7 +1,20 @@
 #include "holberton.h"
 /**
-*
+* print_chessboard - prints a chess board
+* @a: the array that contains the board toys
 *Description:
-*Return:
+*Return: nothing
 */
 
+void print_chessboard(char (*a)[8])
+{
+int i;
+while (a[i])
+{
+_putchar('a[i]');
+if (i % 8 == 0)
+{
+_putchar('\n');
+}
+}
+}
