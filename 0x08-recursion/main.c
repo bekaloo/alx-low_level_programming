ac4ce9cc305695ddb8620b75185502e0e749ea85
Sankaratom <bekalutilahun39@gmail.com>
2021-07-07 12:22:15 +0300

    Recursion Recursion hot dog and balony

---
+++ 0x08-recursion/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+
+void _puts_recursion(char *s)
+{
+if (*s == '\0')
+{
+putchar('\n');
+}
+else 
+{putchar(*s);
+s++;
+_puts_recursion(s);
+}
+}
+
+
+
+int main(void){
+
+    _puts_recursion("Bekalu");
+
+    return 0;
+}
