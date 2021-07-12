f4059baa5a578dfeb5538d3dfa8f5282941f62e3
Sankaratom <bekalutilahun39@gmail.com>
2021-07-13 02:35:42 +0300

    recursive version

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,24 +1,60 @@
-#include "holberton.h"
 #include <stdio.h>
 #include <stdlib.h>
 /**
 * nochar - main
-* @s: string
+* @s: command count/size of argv
 *Description:
 *Return: 0
 */
+
 int nochar(char *s)
 {
-int i = 0;
-while (s[i])
+if (*s < 48 || *s > 57)
 {
-if (s[i] < 48 || s[i] > 57)
+if (*s == '\0')
+{
+return (0);
+}
+else
 {
 return (1);
 }
-i++;
 }
-return (0);
+else
+{
+s++;
+return (nochar(s));
+}
+}
+/**
+* adder - main
+* @c: command count/size of argv
+* @cnt: array of commands
+* @sum: command count/size of argv
+* @limit: array of commands
+*Description:
+*Return: 0
+*/
+void adder(char *c[], int cnt, int *sum, int limit)
+{
+if (limit <= 1)
+{
+printf("0\n");
+}
+if (cnt < limit)
+{
+if (nochar(c[cnt]) == 0)
+{
+*sum += atoi(c[cnt]);
+cnt++;
+adder(c, cnt, sum, limit);
+}
+else
+{
+*sum = 0;
+printf("Error\n");
+}
+}
 }
 /**
 * main - main
@@ -29,26 +65,11 @@ return (0);
 */
 int main(int argc, char *argv[])
 {
-int i, sum = 0;
-if (argc > 1)
+int sum = 0, i = 1;
+adder(argv, i, &sum, argc);
+if (sum != 0)
 {
-for (i = 1; i < argc; i++)
-{
-if (nochar(argv[i]) == 0)
-{
-sum += atoi(argv[i]);
-}
-else
-{
-printf("Error\n");
-return (1);
-}
-}
 printf("%d\n", sum);
-}
-else
-{
-printf("0\n");
 }
 return (0);
 }
