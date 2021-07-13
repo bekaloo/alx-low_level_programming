46c396509ba105995d75921dd74b9fa5084c71b1
Sankaratom <bekalutilahun39@gmail.com>
2021-07-13 21:39:49 +0300

    str_concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,7 +1,57 @@
 #include "holberton.h"
+#include <stdlib.h>
 /**
-*
+* length - string dupilicator
+* @str: string to be cloned
 *Description:
-*Return:
+*Return: a copy of the string
 */
-
+int length(char *str)
+{
+if (str != NULL)
+{
+int j = 0;
+while (str[j])
+{
+j++;
+}
+return (j);
+}
+else
+{
+return (0);
+}
+}
+/**
+* str_concat - string concatenator
+* @s1: string to be cloned
+* @s2: string to be cloned
+*Description:
+*Return: a concatenated version of two strings
+*/
+char *str_concat(char *s1, char *s2)
+{
+int j = 0, size = length(s1) + length(s2);
+int size1 = length(s1), size2 = length(s2);
+char *s = (char *) malloc(size + 1);
+if (s != NULL)
+{
+while (s[j])
+{
+if (j < size1)
+{
+s[j] = s1[j];
+}
+else
+{
+s[j] = s2[j];
+}
+j++;
+}
+return (s);
+}
+else
+{
+return (NULL);
+}
+}
