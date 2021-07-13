398cdf195d6abbd3cda99744f16a7fdbef649c89
Sankaratom <bekalutilahun39@gmail.com>
2021-07-13 21:09:32 +0300

    string duplicator-- strdup

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,7 +1,23 @@
 #include "holberton.h"
+#include <stdlib.h>
 /**
-*
+* _strdup - string dupilicator
+* @str: string to be cloned
 *Description:
-*Return:
+*Return: a copy of the string
 */
-
+char *_strdup(char *str)
+{
+int i = 0, j = 0;
+while (str[i])
+{
+i++;
+}
+char *s = (char *) malloc(i);
+while (str[j])
+{
+s[j] = str[j];
+j++;
+}
+return (s);
+}
