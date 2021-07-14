796e049fb85061018b7b292c45c711bf237611da
Sankaratom <bekalutilahun39@gmail.com>
2021-07-14 03:47:14 +0300

    freeing multi-dimensional array

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,7 +1,18 @@
 #include "holberton.h"
 /**
-*
+* free_grid - frees a multidimensional array
+* @grid: the matrix/array
+* @height: height of the matrix/array
 *Description:
-*Return:
+*Return: nothing
 */
 
+void free_grid(int **grid, int height)
+{
+int j;
+for (j = 0; j < height; j++)
+{
+free(grid[j]);
+}
+free(grid);
+}
