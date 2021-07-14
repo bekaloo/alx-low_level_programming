2a56ad9c2e3eee520db3dc5a2582c8377ab904b0
Sankaratom <bekalutilahun39@gmail.com>
2021-07-14 05:50:40 +0300

    m-d_a_allocate

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,62 +1,35 @@
 #include "holberton.h"
 #include <stdlib.h>
 /**
-* freemem - frees a multidimensional array
-* @arr: the matrix/array
-* @height: height of the matrix/array
-*Description:
-*Return: nothing
-*/
-void freemem(int **arr, int height)
-{
-int j;
-for (j = 0; j < height; j++)
-{
-free(arr[j]);
-}
-free(arr);
-}
-/**
-* alloc_grid - allocates a multidimensional array
-* @width: width of the matrix/array
-* @height: height of the matrix/array
-*Description:
-*Return: a multidimensional array
-*/
-
+ * alloc_grid - creates a 2d integer grid
+ * @width: number of columns
+ * @height: number of rows
+ * Return: pointer to grid or null
+ **/
 int **alloc_grid(int width, int height)
 {
-int i, j;
-int **arr = (int **) malloc(sizeof(int *) * height);
-if (height <= 0 || width <= 0)
-{
-freemem(arr,  height);
-return (NULL);
-}
-else
-{
-arr = (int **) malloc(sizeof(int *) * height);
-if (arr == NULL)
-{
-freemem(arr,  height);
-return (NULL);
-}
-for (i = 0; i < height; i++)
-{
-arr[i] = (int *) malloc(sizeof(int) * width);
-if (arr[i] == NULL)
-{
-freemem(arr,  height);
-return (NULL);
-}
-}
-for (i = 0; i < height; i++)
-{
-for (j = 0; j < width; j++)
-{
-arr[i][j] = 0;
-}
-}
-return (arr);
-}
+	int **grid, i, j;
+
+	if (width < 1 || height < 1)
+		return (NULL);
+	grid = malloc(sizeof(int *) * height);
+	if (grid == NULL)
+		return (NULL);
+	for (i = 0; i < height; i++)
+	{
+		grid[i] = malloc(sizeof(int) * width);
+		if (grid[i] == NULL)
+		{
+			while (--i >= 0)
+				free(grid[i]);
+			free(grid);
+			return (NULL);
+		}
+	}
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+			grid[i][j] = 0;
+	}
+	return (grid);
 }
