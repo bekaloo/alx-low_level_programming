c5d696b0652d8b242980abbd10c6a8086f9cad89
Sankaratom <bekalutilahun39@gmail.com>
2021-07-19 15:01:49 +0300

    using the __FILE__ macro to print the file name

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,7 +1,13 @@
 #include "holberton.h"
+#include <stdio.h>
 /**
-*
+* main - prints the file name
 *Description:
-*Return:
+*Return: 0
 */
 
+int main(void)
+{
+printf(__FILE__"\n");
+return (0);
+}
