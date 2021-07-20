8b8eb566a6e65b254b26733206ecac18df72edf5
Sankaratom <bekalutilahun39@gmail.com>
2021-07-20 05:28:14 +0300

    new dog creator

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -9,13 +9,16 @@
 */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-if (name != NULL && owner != NULL)
+dog_t *nd = malloc(sizeof(dog_t));
+if (!nd)
 {
-dog_t *nd;
+return (NULL);    
+}
+else
+{
 nd->name = name;
 nd->age = age;
 nd->owner = owner;
 return (nd);
 }
-return (NULL);
 }
