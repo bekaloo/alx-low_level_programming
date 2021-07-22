e0fb78b844b0f047e2c84d895b2cb2461936816d
Sankaratom <bekalutilahun39@gmail.com>
2021-07-22 04:54:14 +0300

    midnight

--- 0x0E-structures_typedef/test.c
+++ 0x0E-structures_typedef/test.c
@@ -1,21 +1,12 @@
 #include "dog.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include "1-init_dog.c"
-#include "2-print_dog.c"
-/**
- * main - check the code for ALX School students.
- *
- * Return: Always 0.
- */
-
+#include "4-new_dog.c"
 int main(void)
 {
-    struct dog my_dog;
+    dog_t *my_dog;
 
-    my_dog.name = NULL;
-    my_dog.age = 3.5;
-    my_dog.owner = NULL;
-    print_dog(&my_dog);
+    my_dog = new_dog(NULL, 3.5, "Bob");
+    printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog->name, my_dog->age);
     return (0);
 }
