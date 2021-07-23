6bdb79978d2c7c61da018b2069f67cab55da16ba
Sankaratom <bekalutilahun39@gmail.com>
2021-07-23 03:28:25 +0300

    number printer

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,7 +1,22 @@
 #include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
 /**
-*
+* print_numbers - prints numbers with a separator in between
+* @separator: separates the numbers
+* @n: number of numbers to be printed
 *Description:
-*Return:
+*Return: nothing
 */
 
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+unsigned int count = n;
+va_list x;
+va_start(x, n);
+while (count)
+{
+printf("%d%c", *separator, va_arg(x, int));
+count--;
+}
+}
