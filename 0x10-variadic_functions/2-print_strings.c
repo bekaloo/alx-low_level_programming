bf28778fdedc8e6b847ca2caff7909b7d1b13058
Sankaratom <bekalutilahun39@gmail.com>
2021-07-23 19:11:03 +0300

    string printer

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,7 +1,36 @@
 #include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
 /**
-*
-*Description:
+* print_strings - prints numbers with a separator in between
+* @separator: separates the numbers
+* @n: number of numbers to be printed
 *Return:
 */
-
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+unsigned int count = n;
+va_list x;
+va_start(x, n);
+while (count)
+{
+char *temp = va_arg(x, char *);
+if (temp != NULL)
+{
+if (count > 1)
+{
+printf("%s%s", temp, separator);
+}
+else
+{
+printf("%s\n", temp);
+}
+}
+else
+{
+printf("(nil)\n");
+break;
+}
+count--;
+}
+}
