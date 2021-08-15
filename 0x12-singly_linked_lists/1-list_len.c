883757b3c9c7102e98342b4b0b6fed3807c718f6
Sankaratom <bekalutilahun39@gmail.com>
2021-08-15 23:04:08 +0300

    list length

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,7 +1,19 @@
-#include "holberton.h"
+#include "lists.h"
+#include <stddef.h>
 /**
-*
+*list_len - length of a linked list
+* @h: the linked list
 *Description:
-*Return:
+*Return: length of the list
 */
 
+size_t list_len(const list_t *h)
+{
+size_t i;
+while (h)
+{
+i++;
+h = h->next;
+}
+return (i);
+}
