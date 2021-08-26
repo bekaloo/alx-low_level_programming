9439bc1a116f3f78cad5a0df454e56a3b7619332
Sankaratom <bekalutilahun39@gmail.com>
2021-08-26 17:58:52 +0300

    f_i/o

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,6 +1,10 @@
-#include <stdlib.h>
 #include <stdio.h>
 #include "main.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
 /**
 *read_textfile - file reader
 *@filename: file name to be read
@@ -12,21 +16,23 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 
-FILE *fp, *fw;
+int fr, fw, bytes, chars = 0;
 char c;
-fw = fopen(filename, "w");
-fp = fopen(filename, "r");
-if (fp == NULL || filename == NULL)
+fr = open(filename, O_RDONLY);
+fw = open(filename, O_WRONLY | O_CREAT | S_IRUSR, S_IWUSR);
+if (filename == NULL)
 {
 return (0);
 }
 else
 {
-while ((c = fgetc(fp)) != EOF)
+while ((bytes = read(fr, &c, sizeof(c))) > 0)
 {
-fputc(c, fw);
+write(1, &c, 1);
+chars++;
 }
-fclose(fp);
-return (1);
+close(fr);
+close(fw);
+return (chars);
 }
 }
