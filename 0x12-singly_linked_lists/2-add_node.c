60b03854e883ab4d98e2c280c5b5053f333b1650
Sankaratom <bekalutilahun39@gmail.com>
2021-09-12 20:30:17 +0300

    node adder

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,7 +1,44 @@
-#include "holberton.h"
-/**
-*
-*Description:
-*Return:
-*/
+#include "lists.h"
 
+/**
+* str_dup - duplicates a string and returns its length
+* @src: source string
+* @dest: duplicate string
+*Description:
+*Return: length if malloc succeded or -1 if it failed
+*/
+int str_dup(const char *src, char *dest)
+{
+dest = malloc(sizeof(src));
+if (dest)
+{
+int i;
+while (src)
+{
+dest[i] = src[i];
+}
+dest[i] = '\0';
+return (i);
+}
+return (-1);
+}
+/**
+* add_node - adds a node at the beginning of a structure
+* @head: head of the structure to be edited
+* @str: the string data defined in the structure
+*Description:
+*Return: the new head
+*/
+list_t *add_node(list_t **head, const char *str)
+{
+char *dup;
+int len;
+struct list_s *newnode = malloc(sizeof(list_t));
+if (newnode)
+{
+len = str_dup(str, dup);
+newnode->str = dup;
+newnode->next = *head;
+}
+return (newnode);
+}
