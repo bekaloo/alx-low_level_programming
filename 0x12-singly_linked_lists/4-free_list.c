4162ec87734cfca3c227b6ffd239a11ec674a91f
Sankaratom <bekalutilahun39@gmail.com>
2021-09-16 22:15:50 +0300

    free a linked list

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,7 +1,21 @@
-#include "holberton.h"
+#include "lists.h"
+#include <stdlib.h>
 /**
-*
-*Description:
-*Return:
+* free_list - frees memory held by a linked list
+* @head: head of the linked list
+*Return: ---
 */
+void free_list(list_t *head)
+{
+list_t *temp = head;
+while (head->next != NULL)
+{
+temp = head->next;
+free(head->str);
+free(head);
+head = temp;
+}
+free(head->str);
+free(temp);
+}
 
