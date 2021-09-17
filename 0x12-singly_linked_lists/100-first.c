c97b7cbb7d8898952784f10fc63c0f1f9f7c9cea
Sankaratom <bekalutilahun39@gmail.com>
2021-09-18 01:26:23 +0300

    executing before main

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,7 +1,5 @@
-#include "holberton.h"
-/**
-*
-*Description:
-*Return:
-*/
-
+#include "lists.h"
+void __attribute__ ((constructor)) premain()
+{
+printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
